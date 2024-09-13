261772244317e9c5cc0bb5739f9c753cb4de51e0
lovepreetsingh-8 <119654501+lovepreetsingh-8@users.noreply.github.com>
2024-09-13 11:04:37 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
           <li><Link href="/week-6">Week 6</Link></li> 
           <li><Link href="/week-7">Week 7</Link></li> 
           <li><Link href="/week-8">Week 8</Link></li> 
+          <li><Link href="/week-9">Week 9</Link></li> 
         </ul>
         </div>
     </div>
